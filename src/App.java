e242fc1b88d4774459dcb22de76821b7914da623
Luka Cerqueira <104553827+Luka9kw@users.noreply.github.com>
2024-07-23 18:58:30 -0300

    Scope and method (#2)
    * feat: add method was added
    
    * feat: atribute hello was added

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
+    String hello = "Hello, World!";
     public static void main(String[] args) {
         String hello = "Hello, World!";
         System.out.println(hello);
+    }
+
+    void add(){
+        System.out.println("Add two numbers" + hello);
     }
 }
