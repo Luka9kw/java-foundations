17f08bc905bfcc48c517cc75b4cdc9a4336411e2
seven <7b37b3@gmail.com>
2024-07-23 18:03:57 -0300

    chore: move text from sysOut to a variable

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String hello = "Hello, World!";
+        System.out.println(hello);
     }
 }
