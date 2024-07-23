197c5eb43a58ec562c8cbbc412f71bec94d09a16
luka9kw <luka9kw@hotmail.com>
2024-07-23 18:42:58 -0300

    feat: atribute hello was added

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 public class App {
+    String hello = "Hello, World!";
     public static void main(String[] args) {
         String hello = "Hello, World!";
         System.out.println(hello);
     }
-    
+
     void add(){
-        System.out.println("Add two numbers");
+        System.out.println("Add two numbers" + hello);
     }
 }
